b87afc5e8d0d6b4e79d297077783421c4485f042
OHerrmann92 <O.herrmann92@gmail.com>
2015-04-23 10:25:10 +0200

    minor changes

--- src/de/herrmanno/simple_web/plugins/html/HTML_TypeHandler.java
+++ src/de/herrmanno/simple_web/plugins/html/HTML_TypeHandler.java
@@ -1,5 +1,6 @@
 package de.herrmanno.simple_web.plugins.html;
 
+import de.herrmanno.simple_web.plugins.file.File;
 import de.herrmanno.simple_web.plugins.file.File_TypeHandler;
 
 public class HTML_TypeHandler extends File_TypeHandler {
@@ -7,4 +8,11 @@ public class HTML_TypeHandler extends File_TypeHandler {
 	public HTML_TypeHandler(String dir) {
 		super(dir);
 	}
+
+	@Override
+	public Class<? extends File> getHandledType() {
+		return HTML.class;
+	}
+
+	
 }
